e482cd111bc364140c0d576f8c4e7a0f6dbfe827
Max Pistawka <mpistawk@students.cs.ubc.ca>
2023-07-25 23:07:29 -0700

    third commit

---
+++ src/apis.ts
@@ -0,0 +1,3 @@
+export const weatherKey:string = "d4efb85fbdf2a6e57854a1d17629c5b7"
+export const idKey:string = "8cb4ff86315d4c4ebffdcfe12e853eff"
+export const secretKey = "67bb9b0db3f642539a2a8b1547956833"
